ceb14acf26a7dc8c89e72ac73e994750faec5b0e
Titcho02 <test.tewfik@gmail.com>
2020-01-08 05:33:13 -0500

    log4j depricated

--- RaghavSelenium/src/test/java/listeners/TestNgListeners.java
+++ RaghavSelenium/src/test/java/listeners/TestNgListeners.java
@@ -6,43 +6,36 @@ import org.testng.ITestResult;
 
 public class TestNgListeners implements ITestListener {
 
-	@Override
 	public void onTestStart(ITestResult result) {
 		System.out.println("********* Test started : "+result.getName());
 		
 	}
 
-	@Override
 	public void onTestSuccess(ITestResult result) {
 		System.out.println("********* Test seccesful : "+result.getName());
 		
 	}
 
-	@Override
 	public void onTestFailure(ITestResult result) {
 		System.out.println("********* Test failed : "+result.getName());
 		
 	}
 
-	@Override
 	public void onTestSkipped(ITestResult result) {
 		System.out.println("********* Test skipped : "+result.getName());
 		
 	}
 
-	@Override
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void onStart(ITestContext context) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void onFinish(ITestContext context) {
 		System.out.println("********* Test is completed : "+context.getName());
 		
